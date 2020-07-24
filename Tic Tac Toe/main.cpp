cf4aa82b7cf145c77f01e68d93f0ca33fc39ce21
Maxwell Melnik <maxwellmelnik@gmail.com>
2020-07-24 11:42:20 -0400

    Initial Commit

---
+++ Tic Tac Toe/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Tic Tac Toe
+//
+//  Created by Max on 7/24/20.
+//  Copyright © 2020 Max CSM HW. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
